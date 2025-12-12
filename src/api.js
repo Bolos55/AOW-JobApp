bf616eb703993b506ae04a086a3819df78b25a7c
Bolos55 <123412026+Bolos55@users.noreply.github.com>
2025-12-12 09:36:08 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,10 @@
-// src/api.js
 const isLocal =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1";
 
 export const API_BASE = isLocal
-  ? "http://localhost:5000" // หรือ port ที่ backend รันตอน dev จริง ๆ
-  : "https://aow-jobapp-backend.onrender.com"; // URL backend บน Render (ไม่ต้องต่อ /api)
+  ? "http://localhost:5000"  // backend ตอน dev
+  : "https://aow-jobapp.onrender.com"; // backend บน Render
 
 export const authHeader = () => {
   const token = localStorage.getItem("token");
